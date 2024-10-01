037298247161d900904be8ae9a8ece9c913bdc81
haema41 <52439031+haema41@users.noreply.github.com>
2024-10-01 18:51:37 +0530

    Add files via upload

---
+++ NetBeansProjects/ui-dashboard001/src/main/java/com/surya/ui/dashboard001/UiDashboard001.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.surya.ui.dashboard001;
+
+/**
+ *
+ * @author PC
+ */
+public class UiDashboard001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
